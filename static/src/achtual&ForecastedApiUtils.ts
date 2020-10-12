f7e42f1794b2e1fcd0d54feaa2c3f2465fe91ed8
Akash Verma <akashv1507@gmai.com>
2020-10-13 01:03:09 +0530

    minor bug fixed

--- static/src/achtual&ForecastedApiUtils.ts
+++ static/src/achtual&ForecastedApiUtils.ts
@@ -1,10 +1,10 @@
 import { convertDateTimeToUrlStr } from "./timeUtils";
 import {DataFromApi} from "./index"
 
-export const getActualForecastedDemand = async (entityTag: string, startDate: Date, endDate: Date) => {
+export const getActualForecastedDemand = async (entityTag: string, startDate: Date, endDate: Date): Promise<DataFromApi | []> => {
     try {
-        const startDateStr = convertDateTimeToUrlStr(startDate)
-        const endDateStr = convertDateTimeToUrlStr(endDate)
+        const startDateStr = convertDateTimeToUrlStr(startDate);
+        const endDateStr = convertDateTimeToUrlStr(endDate);
         const resp = await fetch(`/api/${entityTag}/${startDateStr}/${endDateStr}`, {
             method: 'get'
         });
