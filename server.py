e9e641dd4089c2abfced02a7f51441f8e8c2aea6
Akash Verma <akashv1507@gmai.com>
2020-10-19 13:50:03 +0530

    minor changes

--- server.py
+++ server.py
@@ -73,6 +73,6 @@ def home():
 if __name__ == '__main__':
     serverMode: str = appConfig['mode']
     if serverMode.lower() == 'd':
-        app.run(host="localhost", port=int(appConfig['flaskPort']), debug=True)
+        app.run(host="0.0.0.0", port=int(appConfig['flaskPort']), debug=True)
     else:
         serve(app, host='0.0.0.0', port=int(appConfig['flaskPort']), threads=1)
